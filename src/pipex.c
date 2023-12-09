15b18526063a22376f4f15939f40f675aa6cdfa2
yaharkat <yaharkat@c4r9p4.1337.ma>
2023-12-09 16:50:28 +0100

    Need to fix a leak when passing incorect file name or non existing command

--- src/pipex.c
+++ src/pipex.c
@@ -6,29 +6,29 @@
 /*   By: yaharkat <yaharkat@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 21:19:30 by yaharkat          #+#    #+#             */
-/*   Updated: 2023/12/09 16:27:33 by yaharkat         ###   ########.fr       */
+/*   Updated: 2023/12/09 16:36:49 by yaharkat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 #include "../includes/pipex.h"
 
-void	ft_init_pipex(int ac, char **av, char **envp, t_pipex *pipe_d)
+void ft_init_pipex(int ac, char **av, char **envp, t_pipex *pipe_d)
 {
 	pipe_d->cmd_count = 2;
 	pipe_d->cmd_offset = 2;
 	pipe_d->cmd_iter = 0;
+	pipe_d->env_paths = ft_parse_env_paths(ac, envp, pipe_d);
+	pipe_d->cmd_args = ft_parse_args(ac, av, pipe_d);
+	pipe_d->cmd_paths = ft_parse_paths(ac, pipe_d);
 	pipe_d->in_fd = open(av[1], O_RDONLY, 0644);
 	pipe_d->out_fd = open(av[ac - 1], O_RDWR | O_TRUNC | O_CREAT, 0644);
 	if (pipe_d->out_fd < 0 || pipe_d->in_fd < 0)
 	{
-		perror("Error: while opening files");
 		ft_cleanup_pipe(pipe_d);
+		perror("Error: while opening files");
 		exit(EXIT_FAILURE);
 	}
-	pipe_d->env_paths = ft_parse_env_paths(ac, envp, pipe_d);
-	pipe_d->cmd_args = ft_parse_args(ac, av, pipe_d);
-	pipe_d->cmd_paths = ft_parse_paths(ac, pipe_d);
 	if (pipe(pipe_d->fd_pipe) < 0)
 	{
 		perror("Pipe Error");
@@ -37,9 +37,9 @@ void	ft_init_pipex(int ac, char **av, char **envp, t_pipex *pipe_d)
 	}
 }
 
-void	ft_exec(t_pipex *pipe_d, char **envp)
+void ft_exec(t_pipex *pipe_d, char **envp)
 {
-	pid_t	pid;
+	pid_t pid;
 
 	pid = fork();
 	if (pid < 0)
@@ -52,8 +52,8 @@ void	ft_exec(t_pipex *pipe_d, char **envp)
 	{
 		ft_init_pipe(pipe_d);
 		execve(pipe_d->cmd_paths[pipe_d->cmd_iter],
-			pipe_d->cmd_args[pipe_d->cmd_iter], envp);
-		perror("Error: while executing command");
+			   pipe_d->cmd_args[pipe_d->cmd_iter], envp);
+		ft_fprintf(2, "Error: while executing command %d: %s\n", pipe_d->cmd_iter + 1, strerror(errno));
 		ft_cleanup_pipe(pipe_d);
 		exit(EXIT_FAILURE);
 	}
@@ -64,14 +64,14 @@ void	ft_exec(t_pipex *pipe_d, char **envp)
 	}
 }
 
-int	main(int ac, char **av, char **envp)
+int main(int ac, char **av, char **envp)
 {
-	t_pipex	*pipe_d;
+	t_pipex *pipe_d;
 
 	if (ac != 5)
 	{
 		ft_fprintf(2,
-			"Invalid Arguments: > ./pipex file1 cmd1 cmd2 file2\n");
+				   "Invalid Arguments: > ./pipex file1 cmd1 cmd2 file2\n");
 		exit(EXIT_FAILURE);
 	}
 	pipe_d = malloc(sizeof(t_pipex));
